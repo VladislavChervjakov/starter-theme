c2f69e62ef2cb609d88039549141922449502b00
Vlad9999 <vladchervjakov@gmail.com>
2021-12-16 22:18:39 +0200

    added autoload, added enqueue styles, added bootstrap dependency+

---
+++ wp-content/themes/starter-theme/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e637c6d80041aa044ef0eb51a0f36d32edcaf069',
+        'name' => 'vladislavcervakov/starter-theme',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vladislavcervakov/starter-theme' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e637c6d80041aa044ef0eb51a0f36d32edcaf069',
+            'dev_requirement' => false,
+        ),
+    ),
+);
