daaee49611bc0f96d0f07cbdc2ea1910f81c3396
Vlad9999 <vladchervjakov@gmail.com>
2021-12-16 23:01:49 +0200

    deleted bootstrap from dependencies, added bootstrap via cdn

--- wp-content/themes/starter-theme/inc/Enqueue.php
+++ wp-content/themes/starter-theme/inc/Enqueue.php
@@ -8,26 +8,44 @@ final class Enqueue {
 
 
     private function __construct() {
-       // add_action( 'admin_enqueue_scripts', [ $this, 'starter_theme_load_admin_scripts' ] );
 
         add_action( 'wp_enqueue_scripts', [ $this, 'starter_theme_load_scripts' ] );
 
     }
 
-
-//    public function starter_theme_load_admin_scripts( $hook ) {
-//
-//    }
-
     public function starter_theme_load_scripts () {
-//        wp_register_style(
-//         'starter_theme_style',
-//            get_template_directory_uri() . '/dest/css/starter-theme.css',
-//            [],
-//            '1.0.0',
-//          'all'
-//        );
-//        wp_enqueue_style( 'starter_theme_style' );
+
+        // load bootstrap
+        wp_enqueue_style(
+            'bootstrap',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+        );
+
+        wp_enqueue_script(
+            'popper-js',
+            'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+            [ 'jquery' ],
+        true
+        );
+
+        wp_enqueue_script(
+        'bootstrap-js',
+           'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
+                [ 'jquery' ],
+            true
+        );
+
+
+        // load theme scripts and styles
+        wp_enqueue_style(
+           'starter_theme_style',
+           get_template_directory_uri() . '/dest/css/starter-theme.css',
+           [],
+           '1.0',
+           true
+
+       );
+
 
         wp_enqueue_script(
             'starter_theme_script',
--- wp-content/themes/starter-theme/index.php
+++ wp-content/themes/starter-theme/index.php
@@ -1,3 +1,8 @@
+<div class="text-muted">Bootstrap</div>
+<div class="alert alert-primary col-md-6" role="alert">
+    A simple primary alert—check it out!
+</div>
+
 <?php
 wp_head();
 
